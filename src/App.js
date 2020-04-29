5468bc415d7f0536e5a4dd6b3ab3f3cde5fd9b5d
NamJinWoo <cjsencks@naver.com>
2020-04-29 16:15:00 +0900

    add Career

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Title from './portfolio/Title'
+import Career from './portfolio/Career'
 
 function App() {
   return (
     <div>
       <Title />
-
+      <Career />
     </div>
   );
 }
