83c46c9ded86491dc02bc6b63377ae07ba3d15a2
NamJinWoo <cjsencks@naver.com>
2020-04-29 16:07:38 +0900

    add Title

---
+++ src/portfolio/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Title(){
+    return (
+        <section className="App">
+            <h3>Software Developer</h3>
+            <h1>남진우</h1>
+        </section>
+    );
+}
+
+export default Title;
