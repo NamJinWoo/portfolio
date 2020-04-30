d88e88850d0856d43dc74fe3255c85661150b97d
NamJinWoo <cjsencks@naver.com>
2020-04-30 18:15:53 +0900

    add css to activity

--- src/portfolio/Activity.js
+++ src/portfolio/Activity.js
@@ -1,18 +1,16 @@
 import React from "react";
+import '../css/Activity.css';
 
 function Activity(){
     return (
-        <section className='container'>
-            <h2 className='title'>Experience</h2>
-            <hr/>
-            <h3>글로벌 인재트랙</h3>
-            <p>기간 : 2018.12 ~ 2019.02</p>
-            <p>퍼듀 대학교 갔다옴.</p>
-
-            <h3>학생회 활동</h3>
-            <p>기간 : 2018.03~2018.12</p>
-            <p>체육부장으로 활동함.</p>
-        </section>
+        <div className="activity_container">
+            <div className="intro_left_content">
+                <p className="intro_title">Activity</p>
+            </div>
+            <div className="intro_right_content">
+                <div className="intro_content">Content</div>
+            </div>
+        </div>
     );
 }
 
